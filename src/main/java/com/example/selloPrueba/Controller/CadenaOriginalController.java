8af595408fb3a98acf174b65d6c8e3346c645812
IvanPrz5 <maperLBR@gmail.com>
2023-03-24 18:57:57 -0600

    chekandoTimbrado

--- src/main/java/com/example/selloPrueba/Controller/CadenaOriginalController.java
+++ src/main/java/com/example/selloPrueba/Controller/CadenaOriginalController.java
@@ -1,62 +1,121 @@
 package com.example.selloPrueba.Controller;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.StringReader;
+import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.GeneralSecurityException;
-import java.security.KeyFactory;
-import java.security.PrivateKey;
 import java.security.Signature;
-import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.Base64;
 
-import javax.swing.text.Document;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Templates;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.ssl.PKCS8Key;
+import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 public class CadenaOriginalController {
 
 	private final String xsltPath = "C:/Users/Propietario/Desktop/sdk-java18/resources/XSLT/cadenaoriginal_3_3.xslt";
-	private final String cfdiPath = "C:/Users/Propietario/Desktop/sdk-java18/resources/CFDI33_Validacion_Servicio/cfdi33_1.xml";
+	private final String cfdiPath = "C:/Users/Propietario/Desktop/sdk-java18/resources/CertificadosDePrueba/prueba.xml";
+	private final String cer = "C:/Users/Propietario/Desktop/sdk-java18/resources/CertificadosDePrueba/CSD_EKU9003173C9.cer";
 	private final String key = "C:/Users/Propietario/Desktop/sdk-java18/resources/CertificadosDePrueba/CSD_EKU9003173C9.key";
-	private final String passKey = "12345678a"; 
+	private final String passKey = "12345678a";
 
-	public String sellarCfdi() throws GeneralSecurityException, TransformerException, IOException{
-		PKCS8Key pkcs8 = new PKCS8Key(Base64.getDecoder().decode(key), passKey.toCharArray());
-		KeyFactory privateKeyFactory = KeyFactory.getInstance("RSA");
-		PKCS8EncodedKeySpec pkcs8Encoded = new PKCS8EncodedKeySpec(pkcs8.getDecryptedBytes());
-		PrivateKey privateKey = privateKeyFactory.generatePrivate(pkcs8Encoded);
-		Signature signature = Signature.getInstance("SHA256withRSA");
-		signature.update(getCadena().getBytes());
-		String firma = new String(Base64.getEncoder().encode(signature.sign()));
-		System.out.println("Firma digital del CFDI:" + firma);
-		return null;
+	private final String pathSave = "C:/Users/Propietario/Desktop/sdk-java18/resources/XSLT/modificado.xml";
+
+	public String getSello() {
+		try {
+			/* Sello */
+			PKCS8Key pkcs8 = new PKCS8Key(Files.readAllBytes(Paths.get(key)), passKey.toCharArray());
+			java.security.PrivateKey pk = pkcs8.getPrivateKey();
+
+			Signature signature = Signature.getInstance("SHA256withRSA");
+			signature.initSign(pk);
+			signature.update(getCadena().getBytes("UTF-8"));
+
+			String selloCfdi = new String(Base64.getEncoder().encode(signature.sign()));
+			System.out.println("Sello:      " + selloCfdi);
+
+			/* Cer a Base64 */
+			byte[] fileContent = FileUtils.readFileToByteArray(new File(cer));
+			String cerBase64 = Base64.getEncoder().encodeToString(fileContent);
+			
+			/* Número de Certificado */
+			FileInputStream is = new FileInputStream(cer);
+			CertificateFactory cf = CertificateFactory.getInstance("X.509");
+			X509Certificate certificado=(X509Certificate)cf.generateCertificate(is);
+			byte[] byteArray= certificado.getSerialNumber().toByteArray(); //obtengo no. de serie
+			String NoCertificado = new String(byteArray);
+			System.out.println("No. Certificado:     " + NoCertificado);
+
+			/* Modifica el xml original y lo guarda */
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
+			Document document = documentBuilder.parse(cfdiPath);
+
+			document.getDocumentElement().normalize();
+			document.getDocumentElement().setAttribute("Sello", selloCfdi);
+			document.getDocumentElement().setAttribute("Certificado", cerBase64);
+			document.getDocumentElement().setAttribute("NoCertificado", NoCertificado);
+			// document.getDocumentElement().setAttribute("Fecha", "2023-03-24T18:54:00");
+
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(pathSave));
+			
+			transformer.transform(source, result);
+			return null;
+		
+		} catch (IOException e) {
+			return getStackError(e);
+		} catch (GeneralSecurityException e) {
+			return getStackError(e);
+		} catch (TransformerException e) {
+			return getStackError(e);
+		} catch (SAXException e) {
+			return getStackError(e);
+		} catch (ParserConfigurationException e) {
+			return getStackError(e);
+		}
 	}
 
-    public String getCadena() throws TransformerException, IOException {
-        File xslt = new File(xsltPath);
+	public String getCadena() throws TransformerException, IOException {
+		File xslt = new File(xsltPath);
 		StreamSource sourceXsl = new StreamSource(xslt);
 		File cfdi = new File(cfdiPath);
 		StreamSource sourceXml = new StreamSource(cfdi);
-		
+
 		TransformerFactory tFactory = TransformerFactory.newInstance();
 		Transformer transformer = tFactory.newTransformer(sourceXsl);
-		System.out.println("Cadena Orignal" +  sourceXml.toString());
-		transformer.transform(sourceXml, new StreamResult(System.out));
-		// transformer.transform(sourceXml, new StreamResult(System.out));
-		return null;
+		StringWriter out = new StringWriter();
+
+		transformer.transform(sourceXml, new StreamResult(out));
+		String cadenaOriginal = out.toString();
+
+		System.out.println("Cadena Orignal: " + cadenaOriginal);
+		return cadenaOriginal;
 	}
+
+	public static String getStackError(Throwable ex) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        ex.printStackTrace(printWriter);
+        return stringWriter.toString();
+    }
 }
--- src/main/java/com/example/selloPrueba/SelloPruebaApplication.java
+++ src/main/java/com/example/selloPrueba/SelloPruebaApplication.java
@@ -1,9 +1,5 @@
 package com.example.selloPrueba;
 
-import java.io.IOException;
-
-import javax.xml.transform.TransformerException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -12,10 +8,10 @@ import com.example.selloPrueba.Controller.CadenaOriginalController;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SelloPruebaApplication {
-	public static void main(String[] args) throws TransformerException, IOException{
+	public static void main(String[] args){
 		SpringApplication.run(SelloPruebaApplication.class, args);
 		
 		CadenaOriginalController cadenaOriginalController = new CadenaOriginalController();
-		cadenaOriginalController.getCadena();
+		cadenaOriginalController.getSello();
 	}
 }
